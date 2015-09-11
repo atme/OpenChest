496405de761ae5fe49ec794d6d1e2c4756205e45
atme <d3ljke@gmail.com>
2015-09-11 17:38:08 +0100

    Added main plugin algorithm

--- Assets/OpenChest/OpenChest.cs
+++ Assets/OpenChest/OpenChest.cs
@@ -3,15 +3,47 @@ using System.Collections;
 
 public class OpenChest : MonoBehaviour {
 
-	public GameObject[] objects;
+	private ArrayList items;
+	private int chances = 0;
+	private GameObject droppedItem;
 
 	// Use this for initialization
 	void Start () {
-	
+		items = new ArrayList();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	private void DropItem () {
+		int random = Random.Range(0, chances);
+		int currentSum = 0;
+		foreach (Hashtable item in items) {
+			int chance = (int)item["chance"];
+			if (currentSum <= random && random < currentSum + chance) {
+				droppedItem = (GameObject)item["object"];
+				break;
+			}
+			currentSum += chance;
+		}
+	}
+
+	public void AddObject (GameObject addedObject, int chance) {
+		Hashtable item = new Hashtable();
+		item.Add("object", addedObject);
+		item.Add("chance", chance);
+		items.Add(item);
+
+		chances += chance;
+	}
+
+	public void StartAnimation () {
+		DropItem();
+	}
+
+	public GameObject GetDroppedItem () {
+		return droppedItem;
 	}
 }
